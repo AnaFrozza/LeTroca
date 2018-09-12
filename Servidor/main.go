9a8462b48fcaa1c7e7ba37ff3ce1afd723c861e9
Andre Monegat <andre.monegat@gmail.com>
2018-09-12 14:58:16 -0300

    Adicionadas rotas ao servidor
    Adicionadas funções de retorno para index, new, score e save score.

--- Servidor/main.go
+++ Servidor/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-func main()  {
-	fmt.Println("Hello")
+	"github.com/gorilla/mux"
+)
+
+type Word struct {
+	palavra string
+	achado  bool
+}
+
+// Adicionando palavras na lista de palavras para teste
+// 5 palavras
+var listaWord = []Word{
+	Word{
+		palavra: "leite",
+	},
+	Word{
+		palavra: "elite",
+	},
+	Word{
+		palavra: "tele",
+	},
+	Word{
+		palavra: "til",
+	},
+	Word{
+		palavra: "lei",
+	},
+	Word{
+		palavra: "ele",
+	},
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/new", New)
+	router.HandleFunc("/score", Score)
+	router.HandleFunc("/scoresave", Save)
+	//log em caso de erros
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+func New(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "new!")
+}
+func Score(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "score!")
+}
+func Save(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "save")
 }
