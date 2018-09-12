74a19b5ec4e72a165804e20c7131e840c973226b
AnaFrozza <annafrozza@gmail.com>
2018-09-11 22:34:59 -0300

    inserção de palavras na lista manuamente e criacao da funcao compara

--- Cliente/main.go
+++ Cliente/main.go
@@ -2,26 +2,55 @@ package main
 
 import "fmt"
 
-type Words struct {
-	wd [10]string
-	boll [10]string
+type Word struct {
+	palavra string
+	achado bool
+}
+
+// Adicionando palavras na lista de palavras para teste
+// 5 palavras
+var listaWord = []Word{
+	Word {
+		palavra: "leite",
+	},
+	Word {
+		palavra: "elite",
+	},
+	Word {
+		palavra: "tele",
+	},
+	Word {
+		palavra: "til",
+	},
+	Word {
+		palavra: "lei",
+	},
+	Word {
+		palavra: "ele",
+	},
+	
+}
+
+// Compara se a palavra digitada se encontra na lista de palavra
+func comparaWord(sugestao string){
+
 }
 
 //Checar se a palavra digitada existe dentro da palavra inicial
-
 func check(){
-	var palavra string
+	var sugestao string
 
-	for palavra != "q"{
-		fmt.Scanf("%s\n", &palavra)
+	for sugestao != "q"{
+		fmt.Scanf("%s\n", &sugestao)
 		fmt.Println("\x1b[37;1m")
 		fmt.Println("\u001b[2J")
-		fmt.Println("print", palavra)
+		fmt.Println("print", sugestao)
 		
 	}
 }
 
 func main(){
-	//fmt.Println("ola")
+	fmt.Println(listaWord)
+	
 	check()
 }
