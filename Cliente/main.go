08c728e863ae76d0fbd2c3a400d56f51325607d8
AnaFrozza <annafrozza@gmail.com>
2018-09-12 17:38:15 -0300

    muitas funcoes do cliente

--- Cliente/main.go
+++ Cliente/main.go
@@ -1,56 +1,117 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Word struct {
 	palavra string
 	achado bool
 }
 
-// Adicionando palavras na lista de palavras para teste
-// 5 palavras
-var listaWord = []Word{
-	Word {
-		palavra: "leite",
-	},
-	Word {
-		palavra: "elite",
-	},
-	Word {
-		palavra: "tele",
-	},
-	Word {
-		palavra: "til",
-	},
-	Word {
-		palavra: "lei",
-	},
-	Word {
-		palavra: "ele",
-	},
+var score int
+
+
+// Pegar palavra do json
+func getWord() []Word{
+	var listaWord = []Word{
+		Word {
+			palavra: "leite",
+		},
+		Word {
+			palavra: "elite",
+		},
+		Word {
+			palavra: "tele",
+		},
+		Word {
+			palavra: "til",
+		},
+		Word {
+			palavra: "lei",
+		},
+		Word {
+			palavra: "ele",
+		},
+		
+	}
 	
+	return listaWord
 }
 
-// Compara se a palavra digitada se encontra na lista de palavra
-func comparaWord(sugestao string){
 
+func printa(listaPalavra[] Word){
+	
+	fmt.Printf("\u001b[41m")
+	for i := 0; i < len(listaPalavra); i++ {
+		if listaPalavra[i].achado == true {
+			fmt.Printf("\x1b[37;1m")
+			fmt.Printf("\u001b[42m")
+			fmt.Println(listaPalavra[i].palavra)
+			fmt.Printf("\u001b[0m")
+		} else {
+			fmt.Printf("\x1b[37;1m")
+			fmt.Printf("\u001b[41m")
+			for j:= len(listaPalavra[i].palavra); j > 0 ; j -- {
+				fmt.Printf("#")
+			}
+			fmt.Println()
+		}
+	}
+	fmt.Println("\u001b[0m")
 }
 
 //Checar se a palavra digitada existe dentro da palavra inicial
-func check(){
-	var sugestao string
+func check(listaPalavra[] Word ,sugestao string){
+	for i := 0; i < len(listaPalavra); i++ {
+		if sugestao == listaPalavra[i].palavra {
+			listaPalavra[i].achado = true
+			break		
+		}
+	}
+	fmt.Println("Palavra nao encontrada")
+}
 
-	for sugestao != "q"{
+func play(){
+
+	var listaPalavra[] Word = getWord()
+	var sugestao string
+	
+	printa(listaPalavra)
+
+	for sugestao != "/q"{
+		fmt.Println("Digite uma palavra")
 		fmt.Scanf("%s\n", &sugestao)
-		fmt.Println("\x1b[37;1m")
-		fmt.Println("\u001b[2J")
-		fmt.Println("print", sugestao)
+		check(listaPalavra, sugestao)
 		
+		fmt.Println("\u001b[2J")
+		
+		printa(listaPalavra)	
+		
+
 	}
 }
 
+
+// /q sai do jogo
+// /new uma palavra nova
+// /score retorna o score
+
 func main(){
-	fmt.Println(listaWord)
+	var menu string
 	
-	check()
+	fmt.Println("Menu:\n Para começar um jogo digite /new \n Para sair /q")
+	fmt.Scanf("%s\n", &menu)
+	
+	switch menu {
+	case "/new":
+		play()
+	case "/score":
+		fmt.Println("nao ta feito ainda...")
+	}
+	
 }
+	
+
+
+	
