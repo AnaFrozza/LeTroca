211d8608983017adc2ec5d7e8cca6b2ed304c844
AnaFrozza <annafrozza@gmail.com>
2018-09-12 21:07:41 -0300

    alterações cliente

--- Cliente/main.go
+++ Cliente/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	// "math/rand"
 )
 
 // Word contem a palavra e o estado se ela foi encontrada ou não
@@ -38,64 +39,104 @@ func getWord() []Word {
 	return listaWord
 }
 
+
+// printa as palavras na tela
 func printa(listaPalavra []Word) {
 
-	fmt.Printf("\u001b[41m")
+	fmt.Println("\u001b[40;1m") //fundo preto
+	fmt.Printf("\u001b[31;1m") // letra vermelha
 	for i := 0; i < len(listaPalavra); i++ {
 		if listaPalavra[i].achado == true {
-			fmt.Printf("\x1b[37;1m")
-			fmt.Printf("\u001b[42m")
+			fmt.Printf("\x1b[37;1m") //negrito
+			fmt.Printf("\u001b[42m") //fundo verde
 			fmt.Println(listaPalavra[i].palavra)
-			fmt.Printf("\u001b[0m")
+			fmt.Printf("\u001b[0m") // reset
 		} else {
-			fmt.Printf("\x1b[37;1m")
-			fmt.Printf("\u001b[41m")
+			fmt.Printf("\x1b[37;1m") // negrito
+			fmt.Printf("\u001b[31;1m") // letra vermelha
+			fmt.Printf("\u001b[40;1m") // fundo preto
 			for j := len(listaPalavra[i].palavra); j > 0; j-- {
 				fmt.Printf("#")
 			}
 			fmt.Println()
 		}
 	}
-	fmt.Println("\u001b[0m")
+	fmt.Printf("\u001b[0m") // reset
 }
 
 //Checar se a palavra digitada existe dentro da palavra inicial
-func check(listaPalavra []Word, sugestao string) {
+func check(listaPalavra []Word, sugestao string) string{
+	if sugestao == "/q" {
+		return "sair"
+	}
+
 	for i := 0; i < len(listaPalavra); i++ {
 		if sugestao == listaPalavra[i].palavra {
+			if listaPalavra[i].achado == true {
+				return "duplicado"
+			}
 			listaPalavra[i].achado = true
-			break
+			return "achei"
 		}
 	}
-	fmt.Println("Palavra nao encontrada")
+	return "naoEncontrado"
+}
+
+func randomWord(palavra string) string{
+	
+ 	return palavra
 }
 
 func play() {
 
 	var listaPalavra []Word = getWord()
 	var sugestao string
+	var retorno string
+	var word string = randomWord(listaPalavra[0].palavra)
 
 	printa(listaPalavra)
 
 	for sugestao != "/q" {
+		// fmt.Printf("\u001b#3 \u001b#4") // letra grande
+		fmt.Println(word)
+		// fmt.Printf("\u001b[0m") // reset
 		fmt.Println("Digite uma palavra")
 		fmt.Scanf("%s\n", &sugestao)
-		check(listaPalavra, sugestao)
+		
+		fmt.Printf("\u001b[2J") // limpa a tela
 
-		fmt.Println("\u001b[2J")
+		retorno = check(listaPalavra, sugestao)
+		
+		switch retorno {
+		case "naoEncontrado":
+			fmt.Printf("\u001b[43m") // fundo amarelo
+			fmt.Printf("\u001b[30;1m") // letra petra
+			fmt.Println("-Palavra não encontrada-")
+			fmt.Printf("\u001b[0m") // reset
+		case "duplicado":
+			fmt.Printf("\u001b[43m") // fundo amarelo
+			fmt.Printf("\u001b[30;1m") // letra preta
+			fmt.Println("-Você já encontrou essa palavra-")
+			fmt.Printf("\u001b[0m") // reset
+		}
 
 		printa(listaPalavra)
-
+	
+	}
+	
+	fmt.Printf("\u001b[2J") // limpa a tela
+	
+	for i := 0; i < len(listaPalavra); i++ {
+		fmt.Printf("\x1b[37;1m") //negrito
+		fmt.Printf("\u001b[42m") //fundo verde
+		fmt.Println(listaPalavra[i].palavra)
+		fmt.Printf("\u001b[0m") // reset
 	}
 }
 
-// /q sai do jogo
-// /new uma palavra nova
-// /score retorna o score
-
 func main() {
 	var menu string
-
+	fmt.Printf("\u001b[0m") // reset
 	fmt.Println("Menu:\n Para começar um jogo digite /new \n Para sair /q")
 	fmt.Scanf("%s\n", &menu)
 
@@ -105,5 +146,4 @@ func main() {
 	case "/score":
 		fmt.Println("nao ta feito ainda...")
 	}
-
 }
